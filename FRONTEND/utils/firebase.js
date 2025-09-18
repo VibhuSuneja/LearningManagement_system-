95457aa53f507be08530e11f9f9e73b741188424
VibhuSuneja <myrogstrixg17@gmail.com>
2025-09-18 16:27:09 +0530

    Add Firebase initialization and authentication setup

---
+++ FRONTEND/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "loginlms-5173b.firebaseapp.com",
+  projectId: "loginlms-5173b",
+  storageBucket: "loginlms-5173b.firebasestorage.app",
+  messagingSenderId: "95556680652",
+  appId: "1:95556680652:web:e7efd0bab0f52ad340bb69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
