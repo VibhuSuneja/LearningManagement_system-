091d7ced935f9a58513a0f65cb0d0cd30110c05d
VibhuSuneja <myrogstrixg17@gmail.com>
2025-09-20 10:28:46 +0530

    Refactor userController to improve error handling and code clarity

--- BACKEND/config/cloudinary.js
+++ BACKEND/config/cloudinary.js
@@ -1,2 +1,32 @@
 import { v2 as cloudinary } from 'cloudinary'
 
+
+import fs from 'fs'
+cloudinary.config({ 
+  cloud_name: 'my_cloud_name', 
+  api_key: 'my_key', 
+  api_secret: 'my_secret'
+});
+
+const uploadToCloudinary = async (filePath) => {
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+try {
+    if(!filePath){
+        return null
+    }
+    const uploadResult = await cloudinary.uploader.upload(filePath,
+    {resource_type:"auto"})
+    fs.unlinkSync(filePath)
+    return uploadResult.secure_url
+} catch (error) {
+    fs.unlinkSync(filePath)
+    console.log(error)
+}
+}
+
+export default uploadToCloudinary
--- BACKEND/controller/userController.js
+++ BACKEND/controller/userController.js
@@ -13,3 +13,22 @@ try {
     return res.status(500).json({message:`GetCurrent user error ${error}`})
 }
 }
+
+export const updateProfile = async(req,res) =>{
+    try {
+        const userId = req.userId
+        const {description, name} = req.body
+        let photoUrl
+        if(req.file){
+            photoUrl = await uploadToCloudinary(req.file.path)
+        }
+        const user = await User.findByIdAndUpdate(userId, {name , description , photoUrl})
+
+        if(!user){
+          return res.status(404).json({message:"User not Found"})
+    }
+          return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({message:`Update profile error ${error}`})
+    }
+}
