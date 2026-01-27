9790b59d8fd86934bd24c9c8b78ce45bac340ab9
VibhuSuneja <myrogstrixg17@gmail.com>
2026-01-27 12:01:36 +0530

    Fix: Corrected token generation logic and cookie config for production

--- BACKEND/config/token.js
+++ BACKEND/config/token.js
@@ -1,12 +1,9 @@
-//generating jwt tokens
-import jwt from "jsonwebtoken"
-const genToken= async (userId)=>{ //userId as parameter in this async arrow function
-try{
-    const token = await jwt.sign({userId},process.env. //generating token
-        JWT_SECRET,{expiresIn:"7d"}) //expiry of our token is here 7 days 
-        return token
-} catch(error){
-    console.log(error)
-}
-}
-export default genToken //exporting our function so that it can be used inside authController.js
+import jwt from "jsonwebtoken";
+
+const genToken = async (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET || "fallback_secret", {
+        expiresIn: "7d",
+    });
+};
+
+export default genToken;
